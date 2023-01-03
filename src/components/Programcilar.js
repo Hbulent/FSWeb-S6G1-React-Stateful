d54773387462c2181bde3d45a4c6620215d76c89
Hbulent <bulent5354@gmail.com>
2023-01-03 21:47:16 +0300

    proje tamamlandı

--- src/components/Programcilar.js
+++ src/components/Programcilar.js
@@ -11,10 +11,8 @@ Aynı zaman içinde yalnız bir harika programcıyı öne çıkarabiliriz.
 Yorumları takip edin.
 */
 
-import React from 'react';
+import React, {useState} from 'react';
 /* ADIM 0  */
-
-
 // Bu değişkeni YALNIZCA bir state dilimini başlatmak için kullanın!
 // JSX'te şu anda bu kuralı çiğneyen bir şey var...
 // Export syntaxı, test kitaplığının diziyi içe aktarabilmesi için gereklidir.
@@ -26,26 +24,28 @@ export const enIyilerListesi = [
   { id: '5', isim: 'Frances Allen' },
   { id: '6', isim: 'Carol Shaw' },
 ];
-
 export default function Programcilar() {
+  const [programcilar, setProgramcilar] = useState(enIyilerListesi);
+  const [programciId, setProgramciId] = useState(null);
   // İki state dilimine ihtiyacımız olduğundan, state hooku iki kez kullanmamız gerekecek..
   // Bir yanda programcılar listesi, diğer yanda öne çıkan programcının idsi.
-
-	
+  let isim = "";
   const oneCikaninIsmi = () => {
+    programcilar.forEach((item) => {
+if(programciId == item.id) isim = item.isim
+    })
+    return isim;
     // Bunu sona bırakın!
     // Bu bir event handler değil, yardımcıdır. Kullanımını JSX'te gözlemleyin.
     // Öne çıkan geliştiricinin _isim_ adını döndürmek için her iki state dilimini kullanacak.
-    // Closureların güzelliği, argümanlar yoluyla bilgi enjekte etmeye gerek kalmadan programın 
-	// bu bölgesinden her iki state dilimini de "görebilmemiz"dir.
+    // Closureların güzelliği, argümanlar yoluyla bilgi enjekte etmeye gerek kalmadan programın
+  // bu bölgesinden her iki state dilimini de "görebilmemiz"dir.
   };
-
   const stil = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: 'royalblue', // 🤔 kutlarken renk gold'a dönecek
+    color: programciId == null ? 'royalblue': "gold", // :thinking_face: kutlarken renk gold'a dönecek
   };
-
   return (
     <div className='widget-programmers container'>
       <h2>Programcılar</h2>
@@ -55,9 +55,9 @@ export default function Programcilar() {
           // Şöyle diyebiliriz: "aa bu çalışıyor!" Ama programcilar bir state diliminden gelmiyorsa,
           // asla yeni programci ekleyemeyiz, programcilari düzenleyemeyiz ya da silemeyiz. Düzeltin!
           " */
-          enIyilerListesi.map(dev =>
+          programcilar.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.isim} <button onClick={() => {/* burada dev.id 'yi öne çıkan id'ye atayın */ }}>Kutla</button>
+              {dev.isim} <button onClick={() => {setProgramciId(dev.id) }}>Kutla</button>
             </div>
           )
         }
@@ -67,12 +67,11 @@ export default function Programcilar() {
           // Üçlüler, bir şeyin "gerçekliğine" bağlı olarak "bir şeyi veya diğerini" ifade etmek için harikadır..
           // Sözde-kod: öne çıkan true ise metin 1'i oluşturun, aksi takdirde metin 2'yi oluşturun..
           // Sabit kodlanmış false'u doğru değişkenle değiştirin.
-          false
-            ? `🎉 Hadi ${oneCikaninIsmi()}'ı kutlayalım! 🥳`
+          programciId !== null
+            ? `:tada: Hadi ${oneCikaninIsmi()}'ı kutlayalım! :partying_face:`
             : 'Harika bir programcı seçin'
         }
       </div>
     </div>
   );
 }
- 
